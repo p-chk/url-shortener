f371b70444ccf9678e11a448670d57feca272caa
chayakornkaewlaor <chayakorn.kae@gmail.com>
2022-04-26 22:23:49 +0700

    [N] - add UrlProcessingService class

---
+++ src/main/java/dev/chk/UrlShortener/service/UrlProcessingServiceImpl.java
@@ -0,0 +1,21 @@
+package dev.chk.UrlShortener.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlProcessingServiceImpl implements UrlProcessingService {
+    @Override
+    public String getShortenedUrl() {
+        return null;
+    }
+
+    @Override
+    public void processFullUrl() {
+
+    }
+
+    @Override
+    public String getFullUrl(String shortenedUrl) {
+        return null;
+    }
+}
