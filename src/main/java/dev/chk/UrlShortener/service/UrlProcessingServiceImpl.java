5c18301bf78024ffaf32aa0f12689f2b022cb49b
chayakornkaewlaor <chayakorn.kae@gmail.com>
2022-04-27 21:45:34 +0700

    [N] - add new method

--- src/main/java/dev/chk/UrlShortener/service/UrlProcessingServiceImpl.java
+++ src/main/java/dev/chk/UrlShortener/service/UrlProcessingServiceImpl.java
@@ -19,7 +19,7 @@ public class UrlProcessingServiceImpl implements UrlProcessingService {
         return null;
     }
 
-    private String shortenUrl(String fullUrl) {
-        return null;
+    private void saveUrl() {
+
     }
 }
