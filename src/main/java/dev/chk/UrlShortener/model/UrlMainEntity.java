0ead3b2e890f4a3169973669624956ce25b436a2
chayakornkaewlaor <chayakorn.kae@gmail.com>
2022-04-27 22:32:11 +0700

    [N] - create test case for UrlProcessingServiceImpl processFull

--- src/main/java/dev/chk/UrlShortener/model/UrlMainEntity.java
+++ src/main/java/dev/chk/UrlShortener/model/UrlMainEntity.java
@@ -1,4 +1,23 @@
 package dev.chk.UrlShortener.model;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class UrlMainEntity {
+    private Long id;
+
+    @EqualsAndHashCode.Include
+    private String fullUrl;
+    @EqualsAndHashCode.Include
+    private String shortUrl;
+    private String createdBy;
+    private LocalDateTime createdAt;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedAt;
 }
--- src/test/java/dev/chk/UrlShortener/Service/UrlProcessingServiceImplTest.java
+++ src/test/java/dev/chk/UrlShortener/Service/UrlProcessingServiceImplTest.java
@@ -1,11 +1,13 @@
 package dev.chk.UrlShortener.Service;
 
+import dev.chk.UrlShortener.model.UrlMainEntity;
 import dev.chk.UrlShortener.service.UrlMainService;
 import dev.chk.UrlShortener.service.UrlProcessingServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,9 +16,10 @@ import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class UrlProcessingServiceImplTest {
-
+    @Spy
     @InjectMocks
     UrlProcessingServiceImpl urlProcessingService;
+
 
     @Mock
     UrlMainService urlMainService;
@@ -26,9 +29,21 @@ public class UrlProcessingServiceImplTest {
     void getShortenedUrl_should_returnShortenUrl() {
         doReturn(0).when(urlMainService).countRows();
         String expect = "1";
+
         String actual = urlProcessingService.getShortenedUrl();
 
         verify(urlMainService).countRows();
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
+    void processFullUrl_should_callGetShortenedUrlAndSaveUrlToUrlMainAndReturnShortenedUrl_whenFullUrlIsGiven() {
+        doReturn("1").when(urlProcessingService).getShortenedUrl();
+        String expect = "1";
+        String actual = urlProcessingService.processFullUrl("fullUrl");
+
+        verify(urlProcessingService).getShortenedUrl();
+        verify(urlMainService).save(UrlMainEntity.builder().fullUrl("fullUrl").shortUrl("1").build());
         assertThat(actual).isEqualTo(expect);
     }
 }
