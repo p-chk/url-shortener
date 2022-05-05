8ba7b5ac363d9bd094406c88ddeb14520eba299b
chayakornkaewlaor <chayakorn.kae@gmail.com>
2022-05-05 21:41:39 +0700

    [N] - repo

--- src/main/java/dev/chk/UrlShortener/repository/UrlMainRepository.java
+++ src/main/java/dev/chk/UrlShortener/repository/UrlMainRepository.java
@@ -4,6 +4,9 @@ import dev.chk.UrlShortener.model.UrlMainEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UrlMainRepository extends JpaRepository<UrlMainEntity, Long> {
+    Optional<UrlMainEntity> findByShortUrl(String shortUrl);
 }
